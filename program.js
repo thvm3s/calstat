50363668aa8d8018e1d9ce00eccdc3768b1c3f02
Thames Sookpranee <thvm3s@gmail.com>
2022-12-24 09:38:08 +0700

    Update unit tests and simply getDateRange()

--- program.js
+++ program.js
@@ -9,8 +9,8 @@ function getDateRange(today = new Date()) {
   if (opts['start'] && opts['end']) {
     return [new Date(opts['start']), new Date(opts['end'])];
   }
-  function midnightOf(date, month = today.getMonth(), year = today.getFullYear()) {
-    const result = new Date(today);
+  function midnightOf(date, month, year) {
+    const result = new Date();
     result.setHours(0, 0, 0, 0);
     result.setDate(date);
     result.setMonth(month % 12);
@@ -21,7 +21,7 @@ function getDateRange(today = new Date()) {
     return today.getFullYear() + (today.getMonth() == 11 ? 1 : 0);
   }
   return [
-    midnightOf(1),
+    midnightOf(1, today.getMonth(), today.getFullYear()),
     midnightOf(1, today.getMonth() + 1, getEndYear()),
   ];
 }
--- program.test.js
+++ program.test.js
@@ -1,23 +1,23 @@
 const {getDateRange} = require('./program');
 const {test, expect} = require('@jest/globals');
 
-test('get date range within the same month', () => {
+test('get date range within the same year', () => {
+    const today = new Date("2022-06-20T22:42:16.652Z");
+    const [start, end] = getDateRange(today);
+    expect(start.toLocaleString()).toEqual("6/1/2022, 12:00:00 AM");
+    expect(end.toLocaleString()).toEqual("7/1/2022, 12:00:00 AM");
+});
+
+test('get date range for January', () => {
+    const today = new Date("2022-01-20T22:42:16.652Z");
+    const [start, end] = getDateRange(today);
+    expect(start.toLocaleString()).toEqual("1/1/2022, 12:00:00 AM");
+    expect(end.toLocaleString()).toEqual("2/1/2022, 12:00:00 AM");
+});
+
+test('get date range for December', () => {
     const today = new Date("2022-12-20T22:42:16.652Z");
     const [start, end] = getDateRange(today);
-    expect(start.toLocaleString()).toEqual("12/18/2022, 12:00:00 AM");
-    expect(end.toLocaleString()).toEqual("12/25/2022, 12:00:00 AM");
-});
-
-test('get date range across previous month boundaries', () => {
-    const today = new Date("2023-01-31T22:42:16.652Z");
-    const [start, end] = getDateRange(today);
-    expect(start.toLocaleString()).toEqual("1/29/2023, 12:00:00 AM");
-    expect(end.toLocaleString()).toEqual("2/5/2023, 12:00:00 AM");
-});
-
-test('get date range across next month boundaries', () => {
-    const today = new Date("2023-02-03T22:42:16.652Z");
-    const [start, end] = getDateRange(today);
-    expect(start.toLocaleString()).toEqual("1/29/2023, 12:00:00 AM");
-    expect(end.toLocaleString()).toEqual("2/5/2023, 12:00:00 AM");
+    expect(start.toLocaleString()).toEqual("12/1/2022, 12:00:00 AM");
+    expect(end.toLocaleString()).toEqual("1/1/2023, 12:00:00 AM");
 });
